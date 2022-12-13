a40b70e40f0e4b9a9da5a885b99bc0c8c69c4105
Ana Victoria <anavictoriagomes1810@gmail.com>
2022-12-13 14:06:52 -0300

    Refatorei o codigo, alterei o valor de uma lista em string com JSON.stringify, e salvei os itens de uma array no localStorage

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
+const itens = []
 
-
+// Função para acontecer a criação do novo item
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
+
+// Função para criar um novo item
 function criaElemento(nome, quantidade) {
-    console.log(nome)
-    console.log(quantidade)
 
     const novoItem = document.createElement('li');
     novoItem.classList.add("item")
@@ -17,8 +25,16 @@ function criaElemento(nome, quantidade) {
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = quantidade
 
-    novoItem.appendChild(numeroItem)
+    novoItem.appendChild(numeroItem);
     novoItem.innerHTML += nome
 
-    lista.appendChild(novoItem)
+    lista.appendChild(novoItem);
+
+    const intemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(intemAtual);
+    localStorage.setItem("item", JSON.stringify(intemAtual));
 }
